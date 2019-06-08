06dac52088d016bd05ab9d9cca6657c81b7228e8
Liam Liao <sitaoliao@Liams-MacBook-Air.local>
2019-06-08 23:14:17 +0100

    moved Board class to separate js file

---
+++ javascript/board.js
@@ -0,0 +1,31 @@
+
+class Board {
+
+  state = [];
+
+  callback;
+
+  constructor(width, height, callback){
+      for( let l = height, row; l--; ){
+        row = [];
+        for( let k = width; k--; ){
+          row.push(0)
+        }
+        this.state.push(row)
+      }
+
+      callback = callback;
+  }
+
+  EmitUpdate(){
+    callback(this)
+  };
+
+
+}
+
+let board = new Board(10,20,function(board){
+  console.log(board.state);
+})
+
+console.log(board)
--- javascript/index.js
+++ javascript/index.js
@@ -1,39 +1,4 @@
 
-
-<<<<<<< HEAD
-
-
-class Board {
-
-  state = [];
-
-  callback;
-
-  constructor(width, height, callback){
-      for( let l = height, row; l--; ){
-        row = [];
-        for( let k = width; k--; ){
-          row.push(0)
-        }
-        this.state.push(row)
-      }
-
-      callback = callback;
-  }
-
-  EmitUpdate(){
-    callback(this)
-  };
-
-  
-}
-
-let board = new Board(10,20,function(board){
-  console.log(board.state);
-})
-
-console.log(board)
-=======
 const w = window,
     d = document,
     e = d.documentElement,
