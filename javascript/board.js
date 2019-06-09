92351675a47960f469ff60e5e79921e1bc3d6a85
Alan Panayotov <alanpanayotov@gmail.com>
2019-06-09 03:20:11 +0100

    ok this time resolved merge conflict

--- javascript/board.js
+++ javascript/board.js
@@ -1,16 +1,9 @@
 class Board {
 
   constructor(width, height, callback){
-<<<<<<< HEAD
     this.state = state.SETTLING
     this.map = []
     this.match = 4
-=======
-    this.state = [];
-    this.match = 4;
-    
-
->>>>>>> 1e608f32565ab68e24697f4d3063dee0f4bd2b75
 
     for( let l = height, row; l--; ){
       row = [];
@@ -41,20 +34,11 @@ class Board {
     this.map[y][x] = color;
   }
 
-<<<<<<< HEAD
-  PlaceShape(x,y,blocks){
-    for( let l = blocks.length, block_x, block_y, color; l--; ){
-      block_x = blocks[l][0];
-      block_y = blocks[l][1];
-      color = blocks[l][2];
-      console.log(l)
-=======
   PlaceBlock(x,y,cells){
     for( let l = cells.length, block_x, block_y, color; --l; ){
       block_x = cells[l][0];
       block_y = cells[l][1];
       color = cells[l][2];
->>>>>>> 1e608f32565ab68e24697f4d3063dee0f4bd2b75
       this.SetCell(x+block_x,y+block_y,color);
     }
     this.SettleBoard()
@@ -66,17 +50,10 @@ class Board {
     //interate up the board
     for(let y = this.height, x; y--; ){
       for( x = this.width; x--; ){
-<<<<<<< HEAD
-        //if current block is not empty and the one below it is, move it down
+        //if current cell is not empty and the one below it is, move it down
         if( y < this.height-1 && this.map[y][x] > 0 && this.map[y+1][x] == 0 ){
             this.map[y+1][x] = this.map[y][x]
             this.map[y][x] = 0
-=======
-        //if current cell is not empty and the one below it is, move it down
-        if( y < this.height-1 && this.state[y][x] > 0 && this.state[y+1][x] == 0 ){
-            this.state[y+1][x] = this.state[y][x]
-            this.state[y][x] = 0
->>>>>>> 1e608f32565ab68e24697f4d3063dee0f4bd2b75
             changes = true;
         }
       }
@@ -95,17 +72,10 @@ class Board {
     //interate up the board
     for(let y = this.height, x, block_group; y--; ){
       for( x = this.width; x--; ){
-<<<<<<< HEAD
-        //if current block is not empty and the one below it is, move it down
-        if( this.map[y][x] > 0 ){
-          block_group = this.CheckSurroundingCells(this.map[y][x],[x+","+y],[]);
-=======
-
         //if cell has a block, i.e. is greater than 0
-        if( this.state[y][x] > 0 ){
+        if( this.map[y][x] > 0 ){
           //get surrounding cells that match in color
-          block_group = this.CheckSurroundingCells(this.state[y][x],[x+","+y],[]);
->>>>>>> 1e608f32565ab68e24697f4d3063dee0f4bd2b75
+          block_group = this.CheckSurroundingCells(this.map[y][x],[x+","+y],[]);
 
           //if group of matching blocks is larger than or equal to match property
           if(block_group.length >= this.match){
@@ -121,15 +91,9 @@ class Board {
         }
       }
     }
-<<<<<<< HEAD
     if( changes )
       this.EmitUpdate();
     return changes
-=======
-    if( changes ){
-      this.EmitUpdate()
-    }
->>>>>>> 1e608f32565ab68e24697f4d3063dee0f4bd2b75
   }
 
   //recursive function checks surrounding cells and adds any of the same color to an array
