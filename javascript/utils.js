9edc968e31433f5810c0bcdf4004b1638b9b5d0e
ein <victorpanayotov@gmail.com>
2019-06-09 16:39:42 +0100

    Introduced new util getRotTest

--- javascript/utils.js
+++ javascript/utils.js
@@ -10,21 +10,20 @@ POSSIBLE STATES:
 2 - PAUSE,    generic unresponsive - used for game over screen, etc
 */
 
-/* A block should take in an array of colors and it's rotation, and should output an array of
-    arrays representing the segments of a block, containing an x, y and color value:
+const blocks = {
+/* A block should take arguments: colors: Array, rot: number, and should output:
         return [ [x, y, color], [...], ... ]
 */
-const blocks = {
-
   basic: (colors, rot) => {
     /* Rotation defines a step counter-clockwise from the bottom of the basic block
       rotation numbers demonstrated below, X as origin:
             [2]
-        [1][X][3]
+         [1][X][3]
             [0]
       For odd rotations, (1 - 2) will provide the x offset -1, and (3 - 2) will provide the x offset 1
       For even rotations, (0 - 1) will provide the y offset -1, and (2 - 1) will provide the y offset 1
     */
+
     if (rot % 2 == 0) {
       return [[0, 0, colors[0]], [0, rot-1, colors[1]]]
     } else {
@@ -32,4 +31,17 @@ const blocks = {
     }
   }
     
+}
+
+const getRotTest = {
+  basic: (oldRot, newRot=oldRot) => {
+    const tests = [
+      [ [0, 0], [0, 1] ],         // Rotation 0, index 0
+      [ [0, 0], [1, 0], [0, 1] ], // Rotation 1, index 1
+      [ [0, 0] ],                 // Rotation 2, index 2
+      [ [0, 0], [-1, 0], [0, 1] ] // Rotation 3, index 3
+    ]
+
+    return tests[rot];            // Returned to the board
+  } 
 }
